a099d7258d3e19af82c2c78c282980552261f62f
MacOS <macos@blessing.home>
2023-01-30 18:38:07 +0000

    feat: finishing level 100

--- script.js
+++ script.js
@@ -5,8 +5,30 @@ function setup() {
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  const ul = document.getElementById("episodes-list");
+  for (const episode of episodeList){
+    //creating elements li,p,img,h3
+    const li = document.createElement('li');
+    li.classList.add('card')
+    const name = document.createElement('h3');
+    name.classList.add('name-of-episode')
+    const img = document.createElement('img')
+    const para = document.createElement('p');
+    
+    name.innerText = `${episode.name} - S${episode.season.toString().padStart(2, '0')}E${episode.number.toString().padStart(2, '0')}` 
+    img.src = episode.image.medium
+    para.innerHTML = episode.summary
+    
+    li.appendChild(name)
+    li.appendChild(img)
+    li.appendChild(para)
+
+    ul.appendChild(li)
+
+  }
 }
+
+
 
 window.onload = setup;
