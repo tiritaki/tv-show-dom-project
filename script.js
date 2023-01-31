b9f0c6251c4bc1bdd488079c414fa9950ec0a361
MacOS <macos@blessing.home>
2023-01-31 14:12:02 +0000

    feat: adding counting for episodes

--- script.js
+++ script.js
@@ -4,39 +4,47 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
-
 function makePageForEpisodes(episodeList) {
-
   const ul = document.getElementById("episodes-list");
+ 
 
-  for (const episode of episodeList){
+  for (const episode of episodeList) {
     //creating elements li,p,img,h3
-    const li = document.createElement('li');
-    li.classList.add('card')
-    const name = document.createElement('h3');
-    name.classList.add('name-of-episode')
-    const img = document.createElement('img')
-    const para = document.createElement('p');
-    
-    name.innerText = `${episode.name} - S${episode.season.toString().padStart(2, '0')}E${episode.number.toString().padStart(2, '0')}` 
-    img.src = episode.image.medium
-    para.innerHTML = episode.summary
-    
-    li.appendChild(name)
-    li.appendChild(img)
-    li.appendChild(para)
+    const li = document.createElement("li");
+    li.classList.add("card");
+    const name = document.createElement("h3");
+    name.classList.add("name-of-episode");
+    const img = document.createElement("img");
+    const para = document.createElement("p");
 
-    ul.appendChild(li)
+    name.innerText = `${episode.name} - S${episode.season
+      .toString()
+      .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`;
+    img.src = episode.image.medium;
+    para.innerHTML = episode.summary;
 
-  const searchInput = document.querySelector('.searchTerm')
-  searchInput.addEventListener('input', e => {
-  const value = e.target.value.toLowerCase()
-  
-  const isVisible = episode.name.toLowerCase().includes(value) || episode.summary.toLowerCase().includes(value) 
-    
-  li.classList.toggle('hide', !isVisible)
-  })
+    li.appendChild(name);
+    li.appendChild(img);
+    li.appendChild(para);
 
-}
+    ul.appendChild(li);
+
+    //search bar functionality
+    const searchInput = document.querySelector(".searchTerm");
+    searchInput.addEventListener("input", (e) => {
+      const value = e.target.value.toLowerCase();
+
+      const isVisible =
+        episode.name.toLowerCase().includes(value) ||
+        episode.summary.toLowerCase().includes(value);
+      li.classList.toggle("hide", !isVisible);
+      if(isVisible){li.setAttribute('id', 'visible');}
+    });
+          //episodes display
+          const rootElem = document.getElementById("display");
+          const episodesQuantity = document.querySelectorAll('#visible');
+          console.log(episodesQuantity)
+          rootElem.textContent = `Got / ${episodeList.length} episode(s)`;
+  }
 }
 window.onload = setup;
